877145fc027f62a1bf4f5528a22ad12393bc8884
youdianstudio <dev@youdian.org>
2013-05-25 01:52:15 +0800

    s

--- model.py
+++ model.py
@@ -1,16 +1,17 @@
 #-*- coding:utf-8 -*-
 
-from sqlalchemy.orm import sessionmaker,relationship,backref
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import *
-from sqlalchemy.ext.declarative import declarative_base,declared_attr
+from sqlalchemy.ext.declarative import declarative_base, #declared_attr
 import datetime
 
 from config import *
 
-connectionString="mysql://%s:%s@%s:%s/%s?charset=utf8" \
-                    %(MYSQL_USER,MYSQL_PASS,MYSQL_PORT,str(MYSQL_PORT),MYSQL_DB)
+connectionString="mysql://%s:%s@%s:%s/%s?charset=utf8" %(MYSQL_USER, \
+                    MYSQL_PASS, MYSQL_PORT, str(MYSQL_PORT), MYSQL_DB)
 
-engine=create_engine(connectionString,echo=True,encoding='utf8',convert_unicode=True)
+engine=create_engine(connectionString,echo=True,\
+                    encoding='utf8',convert_unicode=True)
 Base=declarative_base()
 
 class User(Base):
@@ -22,37 +23,59 @@ class User(Base):
     nick=Column(String(30),nullable=False,unique=True)
     created=Column(DateTime,nullable=False,default=datetime.datetime.now())
     updated=Column(DateTime,nullable=False,default=datetime.datetime.now())
-    
+
     def __init__(self,name,auth,nick):
         self.name=name
         self.auth=auth
         self.nick=nick
     def __repr__(self):
         return 'User<%s,%s>' %(self.nick,self.name)
-class WiKi(Base):
-    __tablename__='wiki'
+class Shell(Base):
+    __tablename__='shell'
     id=Column(Integer,primary_key=True)
-    cmd=Column(String(100))
-    des=Column(String(200))
-    example=Column(String(200))
+    shell=Column(String(100))
+    description=Column(String(200))
     parent=Column(Integer,default=0)
     platform=Column(String(20))
-    created=Column(DateTime,dafault=datetime.datetime.now())
-    creator=relationship('User',backref='wiki')
-    
-    def __init__(self,cmd,des,parent=0):
-        self.cmd=cmd
-        self.des=des
-        self.parent=parent
+    #created=Column(DateTime,dafault=datetime.datetime.now())
+    #creator=relationship('User',backref='wiki')
+
+    def __init__(self,shell,des):
+        self.shell=shell
+        self.description=des
     def __repr__(self):
-        return 'WiKi<%s,%s>' %(self.cmd,self.des)
+        return 'WiKi<%s,%s>' %(self.shell,self.description)
 
+class Arg(Base):
+    __tablename__='arg'
+    id=Column(Integer,primary_key=True)
+    arg=Column(String(10))
+    description=Column(String(100))
+    shell_id=relationship('Shell',backref='arg')
+    
+    def __init__(self,arg,des,shell_id):
+        self.arg=arg
+        self.description=des
+        self.shell_id=shell_id
+    
+    def __repr__(self):
+        return 'Arg<%s,%s>' %(self.arg,self.description)
 
+class Usage(Base):
+    __tablename__='usage'
+    id=Column(Integer,primary_key=True)
+    usage=Column(String(50))
+    description=Column(String(100))
+    shell_id=relationship('Shell',backref='usage')
+    
+    def __init__(self,usage,des):
+        self.usage=usage
+        
 def createSession():
     Session=sessionmaker(bind=engine)
     session=Session()
     return session
-    
+
 
 
 
